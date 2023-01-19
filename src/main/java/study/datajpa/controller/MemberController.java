77083a52124be9fe5d05c96c76492ce2de7cd5f8
gyunny <needle12s@gmail.com>
2023-01-20 01:21:17 +0900

    feat : paging versionUp

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,55 @@
+package study.datajpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+import study.datajpa.repository.TeamRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+    private final TeamRepository teamRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+
+//        PageRequest request = PageRequest.of(1, 2);
+//        Page<MemberDto> map = memberRepository.findAll(request)
+//                .map(MemberDto::new);
+
+//        return page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+//        return page.map(member -> new MemberDto(member));
+        return page.map(MemberDto::new);
+//      http://localhost:/members?page=0&size=3&sort=id,desc&sort=username,desc
+    }
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+}
--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -14,7 +14,7 @@ import static jakarta.persistence.FetchType.LAZY;
         query="select m from Member m where m.username = :username"
 )
 @NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
-public class Member extends BaseEntity {
+public class Member {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
