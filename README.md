0dea35783e98bb80bc142b23e0d5f06905c902db
gyunny <needle12s@gmail.com>
2023-01-20 11:13:46 +0900

    wip

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Spring-Jpa-Study
+
+@Transactional(readOnly = true)  
+데이터를 단순히 조회만 하고 변경하지 않는 트랜잭션에서
+``readOnly = true`` 옵션을 사용하면 플러시를 생략해서 약간의 성능 향상을 얻음
--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -14,7 +14,7 @@ import static jakarta.persistence.FetchType.LAZY;
         query="select m from Member m where m.username = :username"
 )
 @NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
-public class Member {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
